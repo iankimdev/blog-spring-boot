fbe4b034d8daa310c2f90f9fdce17a31d46b63e0
ian <ian.kim.dev@gmail.com>
2023-06-19 20:41:04 +0900

    feat(service): Develop PostService for Post logic

---
+++ src/main/java/com/example/springbootblog/services/PostService.java
@@ -0,0 +1,37 @@
+package com.example.springbootblog.services;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.springbootblog.models.Post;
+import com.example.springbootblog.repositories.PostRepository;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository postRepository;
+
+    public Optional<Post> getById(Long id) {
+        return postRepository.findById(id);
+    }
+
+    public List<Post> getAll() {
+        return postRepository.findAll();
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == null) {
+            post.setCreatedAt(LocalDateTime.now());
+        }
+        post.setUpdatedAt(LocalDateTime.now());
+        return postRepository.save(post);
+    }
+
+    public void delete(Post post) {
+        postRepository.delete(post);
+    }
+}
