77fb75cb81b7760ae754b7851e2fa32d9a133e47
ian <ian.kim.dev@gmail.com>
2023-06-20 13:23:39 +0900

    feat(model): Add account association to Post entity

--- src/main/java/com/example/springbootblog/models/Post.java
+++ src/main/java/com/example/springbootblog/models/Post.java
@@ -2,6 +2,7 @@ package com.example.springbootblog.models;
 
 import java.time.LocalDateTime;
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -14,11 +15,14 @@ public class Post{
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
-
     private String title;
-
     @Column(columnDefinition = "TEXT")
     private String body;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "account_id", referencedColumnName = "id", nullable = false)
+    private Account account;
 }
