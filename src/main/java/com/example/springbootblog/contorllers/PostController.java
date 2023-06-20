0d70015bd024dfb28feff8a7415b1819979684c7
ian <ian.kim.dev@gmail.com>
2023-06-20 19:20:03 +0900

    feat(controller): Add createPost method

--- src/main/java/com/example/springbootblog/contorllers/PostController.java
+++ src/main/java/com/example/springbootblog/contorllers/PostController.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.springbootblog.models.Account;
 import com.example.springbootblog.models.Post;
+import com.example.springbootblog.services.AccountService;
 import com.example.springbootblog.services.PostService;
 
 @Controller
@@ -30,5 +34,27 @@ public class PostController {
             return "404";
         }
     }
+    @Autowired
+    private AccountService accountService;
     
+    @GetMapping("/posts/create")
+    public String createPost(Model model) {
+
+        Optional<Account> optionalAccount = accountService.findOneByEmail("user@gmail.com");
+        if (optionalAccount.isPresent()) {
+            Post post = new Post();
+            post.setAccount(optionalAccount.get());
+            model.addAttribute("post", post);
+            return "post_create";
+        } else {
+            return "redirect:/";
+        }
+    }
+
+    @PostMapping("/posts/create")
+    public String createPost(@ModelAttribute Post post) {
+        
+        postService.save(post);
+        return "redirect:/posts/" + post.getId();
+    }
 }
