d931f0ab99b6a749e0b470c8688d6f55b6ac51e3
ian <ian.kim.dev@gmail.com>
2023-06-20 13:29:07 +0900

    chore(repository): Remove @Repository

--- src/main/java/com/example/springbootblog/repositories/AccountRepository.java
+++ src/main/java/com/example/springbootblog/repositories/AccountRepository.java
@@ -1,10 +1,9 @@
 package com.example.springbootblog.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.springbootblog.models.Account;
 
-@Repository
+
 public interface AccountRepository extends JpaRepository<Account, Long>{
 }
