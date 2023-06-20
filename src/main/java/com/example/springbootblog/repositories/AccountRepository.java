6fd9f9047ee10e6b105f069239a1945f78d954f4
ian <ian.kim.dev@gmail.com>
2023-06-20 19:22:11 +0900

    feat(repository): Add findOneByEmailIgnoreCase method

--- src/main/java/com/example/springbootblog/repositories/AccountRepository.java
+++ src/main/java/com/example/springbootblog/repositories/AccountRepository.java
@@ -1,9 +1,13 @@
 package com.example.springbootblog.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.springbootblog.models.Account;
 
 
 public interface AccountRepository extends JpaRepository<Account, Long>{
+
+    Optional<Account> findOneByEmailIgnoreCase(String email);
 }
