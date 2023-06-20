69a15b312a3bd2d996e65a763310986730f23fe4
ian <ian.kim.dev@gmail.com>
2023-06-20 10:10:18 +0900

    feat(controller): Add PostController for individual Post handling

---
+++ src/main/java/com/example/springbootblog/contorllers/PostController.java
@@ -0,0 +1,34 @@
+package com.example.springbootblog.contorllers;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.springbootblog.models.Post;
+import com.example.springbootblog.services.PostService;
+
+@Controller
+public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    @GetMapping("/posts/{id}")
+    public String getPost(@PathVariable Long id, Model model) {
+
+        Optional<Post> optionalPost = this.postService.getById(id);
+
+        if (optionalPost.isPresent()) {
+            Post post = optionalPost.get();
+            model.addAttribute("post", post);
+            return "post";
+        } else {
+            return "404";
+        }
+    }
+    
+}
