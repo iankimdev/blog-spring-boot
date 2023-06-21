f389347e34ce7330466f8c977e6547a80c72f5f2
ian <ian.kim.dev@gmail.com>
2023-06-21 09:54:09 +0900

    feat(config): Add user and admin authorities to seed data

--- src/main/java/com/example/springbootblog/config/SeedData.java
+++ src/main/java/com/example/springbootblog/config/SeedData.java
@@ -1,13 +1,17 @@
 package com.example.springbootblog.config;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.example.springbootblog.models.Account;
+import com.example.springbootblog.models.Authority;
 import com.example.springbootblog.models.Post;
+import com.example.springbootblog.repositories.AuthorityRepository;
 import com.example.springbootblog.services.AccountService;
 import com.example.springbootblog.services.PostService;
 
@@ -20,12 +24,23 @@ public class SeedData implements CommandLineRunner{
     @Autowired
     private AccountService accountService;
 
+    @Autowired
+    private AuthorityRepository authorityRepository;
+
     @Override
     public void run(String... args) throws Exception {
         List<Post> posts = postService.getAll();
 
         if (posts.size() == 0) {
+            
+            Authority user = new Authority();
+            user.setName("ROLE_USER");
+            authorityRepository.save(user);
 
+            Authority admin = new Authority();
+            admin.setName("ROLE_ADMIN");
+            authorityRepository.save(admin);
+            
             Account account1 = new Account();
             Account account2 = new Account();
 
@@ -33,11 +48,20 @@ public class SeedData implements CommandLineRunner{
             account1.setLastName("user_last");
             account1.setEmail("user@gmail.com");
             account1.setPassword("password");
-
+            
+            Set<Authority> authorities1 = new HashSet<>();
+            authorityRepository.findById("ROLE_USER").ifPresent(authorities1::add);
+            account1.setAuthorities(authorities1);
+            
             account2.setFirstName("admin_first");
             account2.setLastName("admin_last");
             account2.setEmail("admin@gmail.com");
             account2.setPassword("password");
+            
+            Set<Authority> authorities2 = new HashSet<>();
+            authorityRepository.findById("ROLE_ADMIN").ifPresent(authorities2::add);
+            authorityRepository.findById("ROLE_USER").ifPresent(authorities2::add);
+            account2.setAuthorities(authorities2);
 
             accountService.save(account1);
             accountService.save(account2);
