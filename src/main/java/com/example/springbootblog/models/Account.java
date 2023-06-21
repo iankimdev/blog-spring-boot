523d07d2252c8bbd417fc97d48f0096b2850a0ab
ian <ian.kim.dev@gmail.com>
2023-06-21 09:54:50 +0900

    feat(model): Add authorities field and toString method

--- src/main/java/com/example/springbootblog/models/Account.java
+++ src/main/java/com/example/springbootblog/models/Account.java
@@ -1,7 +1,10 @@
 package com.example.springbootblog.models;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 import javax.persistence.*;
 
 import lombok.Getter;
@@ -24,4 +27,22 @@ public class Account implements Serializable{
 
     @OneToMany(mappedBy = "account")
     private List<Post> posts;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "account_authority",
+            joinColumns = {@JoinColumn(name = "account_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "name")})
+    private Set<Authority> authorities = new HashSet<>();
+
+    @Override
+    public String toString() {
+        return "Account{" +
+            "id=" + id +
+            ", firstName='" + firstName + "'" +
+            ", lastName='" + lastName + "'" +
+            ", email='" + email + "'" +
+            ", authorities=" + authorities +
+        "}";
+    }
 }
