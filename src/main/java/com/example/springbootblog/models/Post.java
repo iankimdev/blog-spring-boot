984df2c6ccd8da5d72d218e98927d2045577175c
ian <ian.kim.dev@gmail.com>
2023-06-22 11:33:12 +0900

    feat(model): Override toString method in Post class

--- src/main/java/com/example/springbootblog/models/Post.java
+++ src/main/java/com/example/springbootblog/models/Post.java
@@ -25,4 +25,15 @@ public class Post{
     @ManyToOne
     @JoinColumn(name = "account_id", referencedColumnName = "id", nullable = false)
     private Account account;
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + "'" +
+                ", body='" + body + "'" +
+                ", createdAt='" + createdAt + "'" +
+                ", updatedAt='" + updatedAt + "'" +
+                "}";
+    }
 }
