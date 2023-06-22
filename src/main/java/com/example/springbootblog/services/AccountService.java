02179775b8f649f64f228ccea7dc48a34cecf236
ian <ian.kim.dev@gmail.com>
2023-06-22 11:33:40 +0900

    feat(service): Add password encoder and authority repository injection

--- src/main/java/com/example/springbootblog/services/AccountService.java
+++ src/main/java/com/example/springbootblog/services/AccountService.java
@@ -1,12 +1,17 @@
 package com.example.springbootblog.services;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.springbootblog.models.Account;
+import com.example.springbootblog.models.Authority;
 import com.example.springbootblog.repositories.AccountRepository;
+import com.example.springbootblog.repositories.AuthorityRepository;
 
 @Service
 public class AccountService {
@@ -14,7 +19,22 @@ public class AccountService {
     @Autowired
     private AccountRepository accountRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthorityRepository authorityRepository;
+    
     public Account save(Account account) {
+
+        if (account.getId() == null) {
+            if (account.getAuthorities().isEmpty()) {
+                Set<Authority> authorities = new HashSet<>();
+                authorityRepository.findById("ROLE_USER").ifPresent(authorities::add);
+                account.setAuthorities(authorities);
+            }
+        }
+        account.setPassword(passwordEncoder.encode(account.getPassword()));
         return accountRepository.save(account);
     }
 
