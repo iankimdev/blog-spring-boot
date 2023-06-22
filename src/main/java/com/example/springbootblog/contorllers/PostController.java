9d2b3c81020f2883c69f285ab2d55e0c88a334af
ian <ian.kim.dev@gmail.com>
2023-06-22 11:32:57 +0900

    feat(controller): Add Update and Delete with authorization

--- src/main/java/com/example/springbootblog/contorllers/PostController.java
+++ src/main/java/com/example/springbootblog/contorllers/PostController.java
@@ -3,8 +3,10 @@ package com.example.springbootblog.contorllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +22,10 @@ public class PostController {
 
     @Autowired
     private PostService postService;
-
+    
+    @Autowired
+    private AccountService accountService;
+    
     @GetMapping("/posts/{id}")
     public String getPost(@PathVariable Long id, Model model) {
 
@@ -34,8 +39,23 @@ public class PostController {
             return "404";
         }
     }
-    @Autowired
-    private AccountService accountService;
+
+    @PostMapping("/posts/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public String updatePost(@PathVariable Long id, Post post, BindingResult result, Model model) {
+
+        Optional<Post> optionalPost = postService.getById(id);
+        if (optionalPost.isPresent()) {
+            Post existingPost = optionalPost.get();
+
+            existingPost.setTitle(post.getTitle());
+            existingPost.setBody(post.getBody());
+
+            postService.save(existingPost);
+        }
+
+        return "redirect:/posts/" + post.getId();
+    }
     
     @GetMapping("/posts/create")
     public String createPost(Model model) {
@@ -56,5 +76,37 @@ public class PostController {
         
         postService.save(post);
         return "redirect:/posts/" + post.getId();
+    }
+
+    @GetMapping("/posts/{id}/update")
+    @PreAuthorize("isAuthenticated()")
+    public String getPostForUpdate(@PathVariable Long id, Model model) {
+
+        // find post by id
+        Optional<Post> optionalPost = postService.getById(id);
+        // if post exist put it in model
+        if (optionalPost.isPresent()) {
+            Post post = optionalPost.get();
+            model.addAttribute("post", post);
+            return "post_update";
+        } else {
+            return "404";
+        }
+    }
+
+    @GetMapping("/posts/{id}/delete")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public String deletePost(@PathVariable Long id) {
+
+        // find post by id
+        Optional<Post> optionalPost = postService.getById(id);
+        if (optionalPost.isPresent()) {
+            Post post = optionalPost.get();
+
+            postService.delete(post);
+            return "redirect:/";
+        } else {
+            return "404";
+        }
     }
 }
