b8bb700ea96d1c88d8d6eff9287c37827cb241cd
ian <ian.kim.dev@gmail.com>
2023-06-22 11:32:13 +0900

    chore(config): Modify post seed data

--- src/main/java/com/example/springbootblog/config/SeedData.java
+++ src/main/java/com/example/springbootblog/config/SeedData.java
@@ -67,13 +67,13 @@ public class SeedData implements CommandLineRunner{
             accountService.save(account2);
 
             Post post1 = new Post();
-            post1.setTitle("What is Lorem Ipsum?");
-            post1.setBody("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+            post1.setTitle("Lorem Ipsum?");
+            post1.setBody("Lorem ipsum dolor sit amet");
             post1.setAccount(account1);
 
             Post post2 = new Post();
-            post2.setTitle("Something else Ipsum");
-            post2.setBody("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Magna eget est lorem ipsum dolor sit amet consectetur adipiscing. Tempus quam pellentesque nec nam aliquam sem et tortor. Pellentesque sit amet porttitor eget. Sed augue lacus viverra vitae congue eu consequat. Ultrices vitae auctor eu augue. Mattis rhoncus urna neque viverra. Consectetur lorem donec massa sapien faucibus et molestie ac feugiat. Sociis natoque penatibus et magnis dis parturient montes nascetur. Cursus turpis massa tincidunt dui ut ornare lectus. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Id cursus metus aliquam eleifend mi. Nibh nisl condimentum id venenatis a condimentum.");
+            post2.setTitle("Ipsum");
+            post2.setBody("Lorem ipsum dolor sit amet");
             post2.setAccount(account2);
             
             postService.save(post1);
